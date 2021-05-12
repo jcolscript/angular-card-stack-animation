4a5951f7c62dea3d3c0317cbd55b323a29d3316d
José Carlos <jcolscript@gmail.com>
2021-05-12 01:47:14 -0300

    card stack animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'cards-stack';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('stack', { static: true })
+  stack!: ElementRef;
+  public advantages = [1, 2, 3, 4];
+
+  ngAfterViewInit() {
+    [...this.stack.nativeElement.children]
+      .reverse()
+      .forEach((i) => this.stack.nativeElement.append(i));
+  }
+
+  next() {
+    let card = this.stack.nativeElement.children[this.advantages.length - 1];
+    card.style.animation = 'changeExit 700ms forwards';
+
+    setTimeout(() => {
+      card.style.animation = '';
+      this.stack.nativeElement.prepend(card);
+    }, 700);
+  }
+
+  prev() {
+    let card = this.stack.nativeElement.children[0];
+    card.style.animation = 'changeEntry 700ms forwards';
+
+    setTimeout(() => {
+      card.style.animation = '';
+      this.stack.nativeElement.append(card);
+    }, 800);
+  }
 }
